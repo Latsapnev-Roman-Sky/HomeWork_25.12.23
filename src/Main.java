a0c9df14afee7afb681a03da7275b892de39df77
latsa <latsapnev01@mail.ru>
2023-12-23 11:14:04 +0300

    Task №1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
-    public static void main(String[] args) { System.out.println("Hello world"); }
+    public static void main(String[] args) {
 
+        //Task 1
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
+}
+
+
